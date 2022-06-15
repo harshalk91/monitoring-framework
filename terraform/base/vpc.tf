9d0a6f3886f1017cb9ac68a60d4846e3c0bfdd1d
Harshal Kulkarni <harshalk.91@gmail.com>
2022-06-15 20:58:07 +0530

    Initial commit - Add base infra and circleci config

---
+++ terraform/base/vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source          = "terraform-aws-modules/vpc/aws"
+  version         = "3.14.0"
+  # insert the 23 required variables here
+  name            = "monitoring-framework-vpc"
+  cidr            = "192.168.1.0/"
+
+  azs             = ["us-east-1"]
+  public_subnets  = ["192.168.1.0/24"]
+  enable_internet_gateway = true
+  enable_bat_gateway  = false
+
+  tags = {
+    terraform = true
+  }
+}
